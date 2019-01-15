391c70b3688c39c7067fb531d158ec328249ae8c
Sakshi Sinha <shrishtisakshi311@gmail.com>
2019-01-15 10:09:12 +0530

    Add files via upload

---
+++ Hadoop/MaxWord.java
@@ -0,0 +1,61 @@
+import java.util.*; 
+import java.io.IOException;
+import org.apache.hadoop.fs.Path; 
+import org.apache.hadoop.conf.*; 
+import org.apache.hadoop.io.*; 
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+public class MaxWord
+{
+   public static class Map extends Mapper <LongWritable, Text, Text, IntWritable>
+   {
+	  IntWritable one=new IntWritable(1);
+	  private Text word=new Text();
+      public void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException 
+      { 
+         String[] line = value.toString().split(",");
+         for(String lines:line) 
+         {
+             context.write(new Text(lines),one);
+          } 
+       } 
+    }
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> 
+    {
+       int max = 0;
+	   Text word = new Text("");
+       public void reduce(Text key,Iterable <IntWritable> values, Context context) throws IOException, InterruptedException 
+       { 
+          int sum=0;
+          for(IntWritable val:values)
+          {
+         	 sum+=val.get();
+          }
+          if(max<sum)
+         {
+           max=sum;word.set(key);
+         }
+       }
+       @Override
+       public void cleanup(Context context) throws IOException, InterruptedException 
+       {
+     	  context.write(new Text(word), new IntWritable(max));
+       }
+       }
+    public static void main(String args[])throws Exception 
+    {
+       Configuration conf=new Configuration();
+       Job job = new Job(conf,"numwords");
+       job.setJarByClass(MaxWord.class); 
+       job.setOutputKeyClass(Text.class);
+       job.setOutputValueClass(IntWritable.class);
+       job.setMapperClass(Map.class);
+       job.setReducerClass(Reduce.class);
+       job.setInputFormatClass(TextInputFormat.class);
+       job.setOutputFormatClass(TextOutputFormat.class);
+       FileInputFormat.addInputPath(job,new Path(args[0]));
+       FileOutputFormat.setOutputPath(job,new Path(args[1]));
+       job.waitForCompletion(true); 
+    } 
+ } 
